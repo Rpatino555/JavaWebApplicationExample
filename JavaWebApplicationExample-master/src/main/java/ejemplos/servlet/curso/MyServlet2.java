7561e41cc625f3d5a92fa9bdf91df542ebe01e18
RPatino555 <rpatinoagudo@outlook.es>
2024-02-12 11:57:38 +0100

    Modificaciones ejercicio 2

---
+++ JavaWebApplicationExample-master/src/main/java/ejemplos/servlet/curso/MyServlet2.java
@@ -0,0 +1,21 @@
+package ejemplos.servlet.curso;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/MyServlet2")
+public class MyServlet2 extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request,
+						 HttpServletResponse response) throws ServletException, IOException {
+
+	request.getRequestDispatcher("/welcome.jsp").forward(request, response);
+
+	}
+
+}
---
+++ JavaWebApplicationExample-master/src/main/webapp/welcome.jsp
@@ -0,0 +1,4 @@
+<html>
+<body>
+<h1>Bienvenido <%=request.getParameter("fname")%></h1>
+</body></html>
